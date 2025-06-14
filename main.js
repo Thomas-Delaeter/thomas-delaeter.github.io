6f864eebe066428b45dfa59f08eff6bf2331de30
thomasdelaeter <thdlaete.delaeter@ugent.be>
2025-06-14 16:36:05 +0200

    feat: disabled chip hover and adjusted skills margin

--- main.js
+++ main.js
@@ -80,7 +80,7 @@ const planetData = [
                             <li>
                             <span class="timeline-dot"></span>
                             <div class="timeline-content">
-                                <strong>EUVIP 2025 conference</strong><br>
+                                <strong>EUVIP 2025 conference submission</strong><br>
                                 "Semi-Supervised Deep Subspace clustering for Hyperspectral Images"
                                 <div class="timeline-details">Jun 2025 | Ghent University</div>
                             </div>
